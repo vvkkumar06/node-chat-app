bb1f2a35ffb4d313b1b1be295b2925c4d5b7176e
Vivek Kumar <vvkkumar06@gmail.com>
2018-03-11 10:37:20 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, err=>{
+    if(err) throw err;
+    console.log("Server running on localhost:"+3000);
+});
