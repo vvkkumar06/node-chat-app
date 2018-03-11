b0b01cc6fff2cb225a18a4a3dda055e22aa2a07f
Vivek Kumar <vvkkumar06@gmail.com>
2018-03-11 12:27:24 +0530

    chat connection functionality

--- server/server.js
+++ server/server.js
@@ -1,11 +1,31 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const app = express();
+
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
 
 app.use(express.static(publicPath));
 
-app.listen(3000, err=>{
-    if(err) throw err;
-    console.log("Server running on localhost:"+3000);
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log("New user connected");
+    socket.on('connect', ()=>{
+        console.log('New user connected')
+    });
+    socket.on('disconnect', ()=>{
+        console.log("User Disconnected");
+    });
 });
+
+server.listen(port, err=>{
+    if(err) throw err;
+    console.log("Server running on localhost:"+port);
+});
+
